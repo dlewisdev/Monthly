b5109ad72df9ba7c48dc061b868160110d582587
Danielle Lewis <103973410+dlewisdev@users.noreply.github.com>
2023-12-23 15:37:04 -0500

    Initial Commit

---
+++ Monthly/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Monthly
+//
+//  Created by Danielle Lewis on 12/23/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Monthly/MonthlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  MonthlyApp.swift
+//  Monthly
+//
+//  Created by Danielle Lewis on 12/23/23.
+//
+
+import SwiftUI
+
+@main
+struct MonthlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
